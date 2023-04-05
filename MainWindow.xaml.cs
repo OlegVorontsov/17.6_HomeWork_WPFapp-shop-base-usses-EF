98974159fa52331da4ef38ed2fe4f898991a093f
Олег Воронцов <oleg-voroncov@mail.ru>
2023-04-05 16:54:11 +0300

    almost done

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.Entity;
 using System.Linq;
 using System.Text;
@@ -20,8 +21,9 @@ namespace _17._6_HomeWork_WPFapp_shop_base_usses_EF
     {
         ClientsDBEntities context;
         static string emailSelected;
-
         bool clientDeleting = false;
+        bool clientChanging = false;
+        bool itemChanging = false;
 
         public MainWindow()
         {
@@ -32,14 +34,13 @@ namespace _17._6_HomeWork_WPFapp_shop_base_usses_EF
         private void Init()
         {
             context = new ClientsDBEntities();
-
             context.ClientsInfo.Load();
             clientsGridView.ItemsSource = context.ClientsInfo.Local.ToBindingList<ClientsInfo>();
         }
         //выбор клиента
         private void clientsGridView_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            if (!clientDeleting)
+            if (!clientDeleting || clientChanging)
             {
                 ClientsInfo clientSelected = (ClientsInfo)clientsGridView.SelectedItem;
                 emailSelected = clientSelected.email;
@@ -91,6 +92,44 @@ namespace _17._6_HomeWork_WPFapp_shop_base_usses_EF
             ItemsInfo itemSelected = (ItemsInfo)itemsGridView.SelectedItem;
             context.ItemsInfo.Remove(itemSelected);
             context.SaveChanges();
+        }
+        //редактирование данных клиента
+        private void clientsGridView_CellEditEnding(object sender, DataGridCellEditEndingEventArgs e)
+        {
+            ClientsInfo clientSelected = (ClientsInfo)clientsGridView.SelectedItem;
+            clientChanging = true;
+        }
+        private void clientsGridView_CurrentCellChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!clientChanging) return;
+                context.SaveChanges();
+                clientChanging = false;
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show($"{err.Message}");
+            }
+        }
+        //редактирование данных товара
+        private void itemsGridView_CellEditEnding(object sender, DataGridCellEditEndingEventArgs e)
+        {
+            ItemsInfo itemSelected = (ItemsInfo)itemsGridView.SelectedItem;
+            itemChanging = true;
+        }
+        private void itemsGridView_CurrentCellChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!itemChanging) return;
+                context.SaveChanges();
+                itemChanging = false;
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show($"{err.Message}");
+            }
         }
     }
 }
